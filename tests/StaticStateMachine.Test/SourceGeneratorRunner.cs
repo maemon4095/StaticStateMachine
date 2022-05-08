f3c106beb518099dc1ba86a1ebef7bc3f71a8765
maemon <47819358+maemon4095@users.noreply.github.com>
2022-05-09 08:18:06 +0900

    runner

--- tests/StaticStateMachine.Test/SourceGeneratorRunner.cs
+++ tests/StaticStateMachine.Test/SourceGeneratorRunner.cs
@@ -7,42 +7,42 @@ namespace StaticStateMachine.Test;
 
 readonly partial struct SourceGeneratorRunner
 {
-    public static SourceGeneratorRunner Create(Config config, IIncrementalGenerator generator)
+    public static SourceGeneratorRunner Create(Config config, Func<IIncrementalGenerator> generatorSource)
     {
-        return new(config, generator);
+        return new(config, generatorSource);
     }
-    public static SourceGeneratorRunner Create(Config config, ISourceGenerator generator)
+    public static SourceGeneratorRunner Create(Config config, Func<ISourceGenerator> generatorSource)
     {
-        return new(config, generator);
+        return new(config, generatorSource);
     }
-    public static SourceGeneratorRunner Create(IIncrementalGenerator generator)
+    public static SourceGeneratorRunner Create(Func<IIncrementalGenerator> generatorSource)
     {
-        return Create(Config.Default, generator);
+        return Create(Config.Default, generatorSource);
     }
-    public static SourceGeneratorRunner Create(ISourceGenerator generator)
+    public static SourceGeneratorRunner Create(Func<ISourceGenerator> generatorSource)
     {
-        return Create(Config.Default, generator);
+        return Create(Config.Default, generatorSource);
     }
 
-    private SourceGeneratorRunner(Config config, object generator)
+    private SourceGeneratorRunner(Config config, Func<object> generator)
     {
         this.config = config;
-        this.generator = generator;
+        this.generatorSource = generator;
     }
 
     readonly Config config;
-    readonly object generator;
+    readonly Func<object> generatorSource;
 
     public RunnerResult Run(string source)
     {
         var config = this.config;
         var syntaxTree = CSharpSyntaxTree.ParseText(source, config.ParseOptions);
         var compilation = CSharpCompilation.Create(config.AssemblyName, new[] { syntaxTree }, config.References, config.CompilationOptions);
-        var driver = this.generator switch
+        var driver = this.generatorSource() switch
         {
             IIncrementalGenerator g => CSharpGeneratorDriver.Create(g),
             ISourceGenerator g => CSharpGeneratorDriver.Create(g),
-            _=> throw new InvalidOperationException()
+            _ => throw new InvalidOperationException()
         };
         if (syntaxTree.GetDiagnostics().Any(d => d.Severity == DiagnosticSeverity.Error)) throw new ArgumentException("Source has syntax error", nameof(source));
         var result = driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out _).GetRunResult();
